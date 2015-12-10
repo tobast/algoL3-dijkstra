27ccab346d8ef6e40b25717c0c5dbabe51a776e9
Noémie Cartier <noemie.cartier@ens.fr>
2015-12-10 08:18:34 +0100

    Corrections

--- extendLists.c
+++ extendLists.c
@@ -10,16 +10,10 @@
 
 #include "extendLists.h"
 
-//FIXME don't force the user to add the first elem
-//FIXME instead, why not let him select the initial size?
-ExtendList* el_create(int val){
-	ExtendList* lst = (ExtendList*)malloc(sizeof(ExtendList)); //FIXME malloc
-	// We don't have to malloc() the structure itself here
-	lst->dispLength = 1;
-	lst->curLength = 1;
-	lst->list = (int*)malloc(sizeof(int));
-	*(lst->list) = val;
-	return lst;
+ExtendList* el_create(int len){
+	return { dispLength = len ; 
+			 curLength = 0 ; 
+			 list = (int*)malloc(len*(sizeof(int)) };
 }
 
 void el_clean(ExtendList* lst){
@@ -28,11 +22,10 @@ void el_clean(ExtendList* lst){
 }
 
 void el_push_back(ExtendList* lst, int val){
-	if (lst->curLength = lst->dispLength){ // FIXME = -> ==
+	if (lst->curLength == lst->dispLength){ 
 		int* lstAux = lst->list;
 		lst->list = (int*)malloc(sizeof(int)*(lst->curLength)*2);
-		int i; // FIXME (nearly) style only, but prefer for(int i=0; ... ; ...)
-		for(i = 0 ; i < (lst->curLength) ; i++)
+		for(int i = 0 ; i < (lst->curLength) ; i++)
 			(lst->list)[i] = (lstAux)[i];
 		free(lstAux);
 		lst->dispLength *= 2;
@@ -45,8 +38,7 @@ void el_pop_back(ExtendList* lst){
 	if ((lst->curLength) - 1 < (lst->dispLength)/4){
 		int* lstAux = lst->list;
 		lst->list = (int*)malloc(sizeof(int)*(lst->dispLength)/2);
-		int i; //FIXME idem push_back
-		for(i = 0 ; i < (lst->curLength) - 1 ; i++)
+		for(int i = 0 ; i < (lst->curLength) - 1 ; i++)
 			(lst->list)[i] = (lstAux)[i];
 		free(lstAux);
 		lst->dispLength /= 2;
@@ -55,14 +47,11 @@ void el_pop_back(ExtendList* lst){
 }
 
 int el_get(ExtendList* lst, int rank){
-	int res = (lst->list)[rank]; //FIXME allocating memory costs time :)
-	// Return it directly (eg. return (a+b))
-	return res;
+	return (lst->list)[rank];
 }
 
 int el_size(ExtendList* lst){
-	int res = lst->curLength; //FIXME
-	return res;
+	return lst->curLength;
 }
 
 
