0181759380fac6cd652b049c45e37745822d017a
Théophile Bastian <contact@tobast.fr>
2015-12-11 16:47:44 +0100

    Created fiboHeap files

---
+++ fiboHeap.h
@@ -0,0 +1,15 @@
+
+/******************************************************************************
+ * Algorithmic project (l3) - Dijkstra algorithm using Fibonacci heap
+ ******************************************************************************
+ * Théophile Bastian <contact@tobast.fr>, Noémie Cartier
+ ******************************************************************************
+ * File: fiboHeap.h
+ * Implements a fibonaccci heap
+ *****************************************************************************/
+
+#include "linkedLists.h"
+
+typedef struct FiboHeap {
+	//TODO
+} FiboHeap;
