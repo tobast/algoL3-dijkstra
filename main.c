62f4b0d2f429ba392c05f9eb76e711e47442a2a4
Théophile Bastian <contact@tobast.fr>
2015-12-15 23:47:46 +0100

    Decent main. Need to implement the real Dijkstra once it will be coded.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "extendLists.h"
 #include "graph.h"
 #include "genericStruct.h"
 #include "naiveDijkstra.h"
@@ -27,22 +28,62 @@ Arguments args(int argc, char** argv) {
 	return out;
 }
 
+void findPath(int pos, int* ancestors, ExtendList* outList) {
+	el_push_back(outList, pos);
+	if(pos != ancestors[pos])
+		findPath(ancestors[pos], ancestors, outList);
+}
+
 int main(int argc, char** argv) {
 	Arguments prgmArgs = args(argc, argv);
 
 	// Input graph
-	//TODO
-
-	// BEHOLD, as Dijkstra itself is executed.
-	if(prgmArgs.useNaiveDijkstra) { // Use the naive array-based version
-		//TODO
+	int nbVert, nbEdges;
+	scanf("%d %d", &nbVert, &nbEdges);
+	Graph graph = g_create(nbVert);
+	graph.nbEdges = nbEdges;
+	for(int edge=0; edge < graph.nbEdges; edge++) {
+		int from,to,weight;
+		scanf("%d %d %d", &from, &to, &weight);
+		g_addEdge(&graph, from, to, weight);
 	}
-	else { // Here comes the real stuff.
-		//TODO
+
+
+	int nbSources;
+	scanf("%d", &nbSources);
+
+	int* distances = (int*) malloc(sizeof(int)*nbVert);
+	int* ancestors = (int*) malloc(sizeof(int)*nbVert);
+	for(int sourceId=0; sourceId < nbSources; sourceId++) {
+		int sourceVert;
+		scanf("%d", &sourceVert);
+
+		// BEHOLD, as Dijkstra itself is executed.
+		if(prgmArgs.useNaiveDijkstra) { // Use the naive array-based version
+			naiveDijkstra(&graph, sourceVert, distances, ancestors);
+		}
+		else { // Here comes the real stuff.
+			//TODO
+			assert(false);
+		}
+
+		// Output path
+		for(int vert=0; vert < nbVert; vert++) {
+			if(distances[vert] < 0)
+				printf("%d: +∞\n", vert);
+			else {
+				printf("%d: %d, ", vert, distances[vert]);
+				ExtendList path = el_create(1);
+				findPath(vert, ancestors, &path);
+				for(int pathPos=path.curLength-1; pathPos > 0; pathPos--)
+					printf("%d -> ", path.list[pathPos]);
+				printf("%d\n", vert);
+			}
+		}
 	}
 
-	// Output path
-	//TODO
+	free(ancestors);
+	free(distances);
 
 	return 0;
 }
