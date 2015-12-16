ad12984ac35dca1077d5c867b2e9701ab3197819
Théophile Bastian <contact@tobast.fr>
2015-12-16 14:27:30 +0100

    Fix typog

--- bench.sh
+++ bench.sh
@@ -8,10 +8,10 @@ for nbVert in `seq 10000 1000 100000`; do
 	if ! [ -f "benchFiles/${nbVert}.in" ]; then
 		python3 ./genRandGraph.py "$nbVert" 2>/dev/null > benchFiles/$nbVert.in
 	fi
-	/usr/bin/time -f "%e" ./dijkstra < benchs/$nbVerts.in \
+	/usr/bin/time -f "%e" ./dijkstra < benchs/$nbVert.in \
 		> /dev/null 2>tests/cur.time
 	tOpt=`cat tests/cur.time`
-	/usr/bin/time -f "%e" ./dijkstra --naive < benchs/$nbVerts.in \
+	/usr/bin/time -f "%e" ./dijkstra --naive < benchs/$nbVert.in \
 		> /dev/null 2>tests/cur.time
 	tNaive=`cat tests/cur.time`
 	rm tests/cur*
