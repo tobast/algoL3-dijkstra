974ec5c4fd00ec0fbca2d516a1826ca0a33be9f5
Théophile Bastian <contact@tobast.fr>
2015-12-16 14:16:20 +0100

    bench.sh keeps its bench files

--- bench.sh
+++ bench.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Generates naive, opti time curves for growing |V| values
 
+mkdir -p "benchFiles/"
 echo -e "nbVert\tnaive\topti"
 
 for nbVert in `seq 10000 1000 100000`; do
-	python3 ./genRandGraph.py "$nbVert" 2>/dev/null > tests/cur.in
-	/usr/bin/time -f "%e" ./dijkstra < tests/cur.in > /dev/null 2>tests/cur.time
+	if ! [ -f "benchFiles/${nbVert}.in" ]; then
+		python3 ./genRandGraph.py "$nbVert" 2>/dev/null > benchFiles/$nbVert.in
+	fi
+	/usr/bin/time -f "%e" ./dijkstra < benchs/$nbVerts.in \
+		> /dev/null 2>tests/cur.time
 	tOpt=`cat tests/cur.time`
-	/usr/bin/time -f "%e" ./dijkstra --naive < tests/cur.in > /dev/null \
-		2>tests/cur.time
+	/usr/bin/time -f "%e" ./dijkstra --naive < benchs/$nbVerts.in \
+		> /dev/null 2>tests/cur.time
 	tNaive=`cat tests/cur.time`
 	rm tests/cur*
 
