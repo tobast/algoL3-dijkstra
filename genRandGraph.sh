cbb704ffa8037d37b6a0fc48057e255a991588fa
Théophile Bastian <contact@tobast.fr>
2015-12-16 13:13:41 +0100

    FIX few things in rand graph generation

--- genRandGraph.sh
+++ genRandGraph.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+noDot=0
+if [ "$1" = "--no-dot" ] ; then
+	noDot=1
+	shift
+fi
+
 python3 ./genRandGraph.py > "$1.in" 2> "$1.dot"
-#dot -Tpng "$1.dot" > "$1.png"
+if (($noDot == 0)); then
+	dot -Tpng "$1.dot" > "$1.png"
+fi
 rm "$1.dot"
